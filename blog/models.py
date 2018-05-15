45d47af02d6202b1f3e303fce10f787199eb019d
KC Steblay <kcsteblay@KCs-MacBook-Pro.local>
2018-05-15 00:27:49 -0700

    Blog and Jobs apps are now live and my homepage has been setup using Bootstrap

--- blog/models.py
+++ blog/models.py
@@ -1,3 +1,13 @@
 from django.db import models
 
-# Create your models here.
+# Create A Blog model
+# Add the Blog app to the settings
+# Create a migration
+# migrate
+# Add to the admin
+
+class Blog(models.Model):
+	image = models.ImageField(upload_to='images/', null=True) #title
+	pub_date = models.DateTimeField(null=True) #publish date
+	title = models.CharField(max_length=255, null=True) #title
+	body = models.TextField(null=True) #body
