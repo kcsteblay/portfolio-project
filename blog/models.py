9a7f462d0f159d2590457658bd33cb26db3f2188
KC Steblay <kcsteblay@KCs-MacBook-Pro.local>
2018-05-15 23:24:49 -0700

    Completed layout and functionality of personal website

--- blog/models.py
+++ blog/models.py
@@ -11,3 +11,12 @@ class Blog(models.Model):
 	pub_date = models.DateTimeField(null=True) #publish date
 	title = models.CharField(max_length=255, null=True) #title
 	body = models.TextField(null=True) #body
+
+	def summary(self):
+		return self.body[:200]
+
+	def pub_date_pretty(self):
+		return self.pub_date.strftime('%b %e %Y')
+
+	def __str__(self):
+		return self.title
